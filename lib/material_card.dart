e964262c8084e24bcfb8964d0adf1b8a2473cac4
Kenneth Endfinger <kaendfinger@gmail.com>
2015-04-06 12:03:24 -0400

    Bug Fixes

--- lib/material_card.dart
+++ lib/material_card.dart
@@ -9,7 +9,7 @@ import 'dart:js' show JsArray, JsObject;
 import 'package:web_components/custom_element_proxy.dart';
 import 'package:web_components/html_import_annotation.dart';
 import 'package:custom_element_apigen/src/common.dart' show PolymerProxyMixin, DomProxyMixin;
-import 'paper_shadow.dart';
+import 'package:paper_elements/paper_shadow.dart';
 
 
 @CustomElementProxy('material-card')
